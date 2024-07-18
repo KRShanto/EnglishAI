91282b0d030e5d577402fbcaad86e15ded01d840
KR Shanto <krshanto2005@gmail.com>
2024-07-18 18:40:49 +0600

    feat: add paragraph page UI

---
+++ src/app/paragraph/Result.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Result({
+  result,
+}: {
+  result: null | "Fetching" | string[];
+}) {
+  if (!result)
+    return (
+      <div className="description">
+        <p>Generate any paragraph/essay you want.</p>
+        <p>
+          You can generate a paragraph about A Cow, about a country, or even
+          generate stories.
+        </p>
+      </div>
+    );
+
+  if (result && result.length > 0)
+    return (
+      <div className="checking-result">
+        <h2 className="result-heading">Result</h2>
+
+        <div className="paragraph-text">
+          {(result as string[]).map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
+        </div>
+      </div>
+    );
+}
---
+++ src/app/paragraph/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+import LoadingAnimation from "@/../public/animations/loading.json";
+import Result from "./Result";
+
+export default function Page() {
+  // TODO: temporary
+  const [input, setInput] = useState("A cow");
+  const [para, setPara] = useState(3);
+
+  const [result, setResult] = useState<"Fetching" | null | string[]>(null);
+
+  return (
+    <div className="paragraph">
+      <div className="heading-div">
+        <h1 className="heading">Paragraph Generator</h1>
+      </div>
+
+      <div className="input-options">
+        <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="input"
+          placeholder="Enter the topic here"
+        />
+
+        <div className="options">
+          <div className="option-para">
+            <button onClick={() => setPara(para + 1)}>+</button>
+            <p>
+              <strong>{para}</strong> {para > 1 ? "Paras" : "para"}
+            </p>
+            <button onClick={() => para !== 1 && setPara(para - 1)}>-</button>
+          </div>
+
+          <button className="option">Generate</button>
+        </div>
+      </div>
+
+      <div className="result">
+        <Result result={result} />
+        <Lottie
+          animationData={LoadingAnimation}
+          className={`loading ${result === "Fetching" && "show"}`}
+          autoplay
+          loop
+        />
+      </div>
+    </div>
+  );
+}
